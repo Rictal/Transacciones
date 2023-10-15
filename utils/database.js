13fc7791fb214edbf466844723cb22986a61a646
Luis <85514882+Rictal@users.noreply.github.com>
2023-10-14 20:08:16 -0700

    Revert "cambios pequeños"

--- utils/database.js
+++ utils/database.js
@@ -1,21 +1,21 @@
-const Mongoose = require("mongoose");
+const Mongoose = require('mongoose');
 
 class Database {
-  connection = null;
-  constructor() {}
+  connection = Mongoose.connection;
+  constructor() {
+  }
   async connect() {
     try {
-      this.connection = await Mongoose.connect(
-        `mongodb+srv://SrJulioDeLana:Rictal98.@cluster0.pfp4y1n.mongodb.net/Transacciones?retryWrites=true&w=majority`
+      await Mongoose.connect(
+        `mongodb+srv://SrJulioDeLana:Rictal98.@cluster0.pfp4y1n.mongodb.net/Transacciones?retryWrites=true&w=majority`,
         /*
         {
           useNewUrlParser: true,
           useUnifiedTopology: true,
         }
         */
+       console.log(`conexion exitosa`)
       );
-
-      console.log(`conexión exitosa`);
     } catch (error) {
       console.error(error);
     }
@@ -23,12 +23,11 @@ class Database {
 
   async close() {
     try {
-      //await this.connection.close();
-      this.connection?.disconnect();
+      await this.connection.close();
     } catch (error) {
       console.error(error);
     }
   }
 }
 
-module.exports = Database;
+module.exports = new Database();
